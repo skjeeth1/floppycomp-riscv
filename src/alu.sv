fc52a9ad82a89a14e7f02e7560c4869619e7e98e
skjeeth1 <skjeeth1@gmail.com>
2025-06-08 23:18:53 +0530

    Fixed stupid error: Used non-blocking in always_comb

--- src/alu.sv
+++ src/alu.sv
@@ -9,22 +9,22 @@ module alu_rtl (
 
     always_comb begin : alu_out
         case (alu_op_code)
-            OP_ALU_ADD: out <= rs1 + rs2;
-            OP_ALU_SUB: out <= rs1 - rs2;
-            OP_ALU_SLL: out <= rs1 << rs2;
-            OP_ALU_SRL: out <= rs1 >> rs2;
-            OP_ALU_SRA: out <= rs1 >>> rs2;
-            OP_ALU_OR: out <= rs1 | rs2;
-            OP_ALU_AND: out <= rs1 & rs2;
-            OP_ALU_XOR: out <= rs1 ^ rs2;
-            OP_ALU_SLTU: out <= (rs1 < rs2) ? 'b1 : 'b0;
-            OP_ALU_SLT: out <= ($signed(rs1) < $signed(rs2)) ? 'b1 : 'b0;
+            OP_ALU_ADD: out = rs1 + rs2;
+            OP_ALU_SUB: out = rs1 - rs2;
+            OP_ALU_SLL: out = rs1 << rs2;
+            OP_ALU_SRL: out = rs1 >> rs2;
+            OP_ALU_SRA: out = rs1 >>> rs2;
+            OP_ALU_OR: out = rs1 | rs2;
+            OP_ALU_AND: out = rs1 & rs2;
+            OP_ALU_XOR: out = rs1 ^ rs2;
+            OP_ALU_SLTU: out = (rs1 < rs2) ? 'b1 : 'b0;
+            OP_ALU_SLT: out = ($signed(rs1) < $signed(rs2)) ? 'b1 : 'b0;
 
             default: begin 
                 out = 'b0; 
                 $error("Invalid ALU op code %x",alu_op_code); 
             end
-            
+
         endcase
     end
 
