44c2e7b84db5eb321d48ead6a614590e07255dc4
skjeeth1 <skjeeth1@gmail.com>
2025-06-14 16:24:22 +0530

    Modified fetch unit

--- src/fetch.sv
+++ src/fetch.sv
@@ -4,21 +4,26 @@ module fetch_unit (
     input logic clock,
     input logic reset,
     input logic jmp_ctrl_signal,
-    input logic [WORD_SIZE - 1:0] jump_address,
-    input logic [WORD_SIZE - 1:0] memory_inst_data,
+    input word jump_address,
+    input word memory_inst_data,
 
-    output mem_op_t mem_op, 
-    output logic [WORD_SIZE - 1:0] memory_inst_address,
-    output logic [WORD_SIZE - 1:0] instruction_out
+    output mem_en_t mem_op, 
+    output word memory_inst_address,
+    output word instruction_out,
+    output word pc_4
 );
 
-    logic [WORD_SIZE - 1:0] pc_reg;
+    word pc_reg;
+
+    always_comb begin
+        pc_4 = pc_reg + 4;
+    end
 
     always_ff @(posedge clock or posedge reset) begin : address_out
         if (reset) begin
             pc_reg <= 'b0;
         end else begin
-            pc_reg <= (jmp_ctrl_signal) ? jump_address : (pc_reg + 4);
+            pc_reg <= (jmp_ctrl_signal) ? jump_address : pc_4;
         end
     end
 
