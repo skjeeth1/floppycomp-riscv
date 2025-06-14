aa21abb2d2f0555fa6220ee5555667e5002ae016
skjeeth1 <skjeeth1@gmail.com>
2025-06-14 18:53:28 +0530

    Reworked register file

--- src/reg_file.sv
+++ src/reg_file.sv
@@ -3,7 +3,7 @@
 module reg_file (
     input logic clock,
     input reg_file_op_t mem_op,
-    input logic [4 : 0] rs1, rs2, rd,
+    input reg_index rs1, rs2, rd,
     input word write_data,
     
     output word rs1_data, rs2_data
@@ -18,17 +18,21 @@ module reg_file (
     end
 
     always_comb begin : Read_Data_RS1
-        if (rs1 == 5'd0)
-            rs1_data = '0;
-        else
-            rs1_data = registers[rs1];
+        if (mem_op == READ_REG_DATA) begin
+            if (rs1 == 5'd0)
+                rs1_data = '0;
+            else
+                rs1_data = registers[rs1];
+        end
     end
 
     always_comb begin : Read_Data_RS2
-        if (rs2 == 5'd0)
-            rs2_data = '0;
-        else
-            rs2_data = registers[rs2];
+        if (mem_op == READ_REG_DATA) begin
+            if (rs2 == 5'd0)
+                rs2_data = '0;
+            else
+                rs2_data = registers[rs2];
+        end
     end
     
 endmodule
