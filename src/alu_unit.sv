24f1e9aa3f79f623ae2f75e2cdcb11d4803e2578
skjeeth1 <skjeeth1@gmail.com>
2025-07-09 11:54:12 +0530

    Restructured branching and alu units

--- src/alu_unit.sv
+++ src/alu_unit.sv
@@ -1,6 +1,7 @@
 `include "params.sv"
 
 module alu_unit (
+    input logic valid,
     input word op1, op2, imm, pc_val,
     input alu_rs1_t alu_rs1,
     input alu_rs2_t alu_rs2,
@@ -11,38 +12,29 @@ module alu_unit (
     word rs1, rs2;
 
     always_comb begin : ALU_IMP
-        rs1 = op1;
-        rs2 = op2;
-        out = 'b0;
-        
-        case (alu_rs1)
-            ALU_PC_OP:  rs1 = pc_val;
-            default:    rs1 = op1;
-        endcase
+        rs1 = (alu_rs1 == ALU_PC_OP) ? pc_val : op1 ;
+        rs2 = (alu_rs2 == ALU_IMM_OP) ? imm : op2;
 
-        case (alu_rs2)
-            ALU_IMM_OP: rs2 = imm;
-            default:    rs2 = op2;
-        endcase
+        if (valid) begin
+            unique case (alu_op_code)
+                NO_ALU_OP: out = imm;
+                OP_ALU_ADD: out = rs1 + rs2;
+                OP_ALU_SUB: out = rs1 - rs2;
+                OP_ALU_SLL: out = rs1 << rs2[4:0];
+                OP_ALU_SRL: out = rs1 >> rs2[4:0];
+                OP_ALU_SRA: out = rs1 >>> rs2[4:0];
+                OP_ALU_OR: out = rs1 | rs2;
+                OP_ALU_AND: out = rs1 & rs2;
+                OP_ALU_XOR: out = rs1 ^ rs2;
+                OP_ALU_SLTU: out = ($unsigned(rs1) < $unsigned(rs2)) ? 'b1 : 'b0;
+                OP_ALU_SLT: out = ($signed(rs1) < $signed(rs2)) ? 'b1 : 'b0;
 
-        case (alu_op_code)
-            NO_ALU_OP: out = imm;
-            OP_ALU_ADD: out = rs1 + rs2;
-            OP_ALU_SUB: out = rs1 - rs2;
-            OP_ALU_SLL: out = rs1 << rs2[4:0];
-            OP_ALU_SRL: out = rs1 >> rs2[4:0];
-            OP_ALU_SRA: out = rs1 >>> rs2[4:0];
-            OP_ALU_OR: out = rs1 | rs2;
-            OP_ALU_AND: out = rs1 & rs2;
-            OP_ALU_XOR: out = rs1 ^ rs2;
-            OP_ALU_SLTU: out = (rs1 < rs2) ? 'b1 : 'b0;
-            OP_ALU_SLT: out = ($signed(rs1) < $signed(rs2)) ? 'b1 : 'b0;
-
-            default: begin 
-                out = 'b0; 
-                $display("ALU: %h %s %h = %h", rs1, alu_op_code, rs2, out);
-            end
-
-        endcase
+                default: begin 
+                    out = 'b0; 
+                    $display("ALU: %h %s %h = %h", rs1, alu_op_code, rs2, out);
+                end
+            endcase
+        end else 
+            out = 32'b0;
     end 
 endmodule
--- src/branch_unit.sv
+++ src/branch_unit.sv
@@ -1,6 +1,7 @@
 `include "params.sv"
 
 module branch_unit (
+    input logic valid,
     input word rs1, rs2, pc, offset,
     input branch_op_t branch_op,
 
@@ -11,52 +12,25 @@ module branch_unit (
     word target_brnch;
 
     always_comb begin : Branch_logic
-        target_brnch = pc + offset;
-        branch_address = '0;
-        branch_scs = BRANCH_DISABLE;
+        if (valid) begin
+            target_brnch = pc + offset;
 
-        case (branch_op)
-            INST_BRCH_EQ: begin
-                if (rs1 == rs2) begin
-                    branch_address = target_brnch;
-                    branch_scs = BRANCH_ENABLE;
-                end
-            end
-            INST_BRCH_NEQ: begin
-                if (rs1 != rs2) begin
-                    branch_address = target_brnch;
-                    branch_scs = BRANCH_ENABLE;
-                end
-            end
-            INST_BRCH_LST: begin
-                if ($signed(rs1) < $signed(rs2)) begin
-                    branch_address = target_brnch;
-                    branch_scs = BRANCH_ENABLE;
-                end
-            end
-            INST_BRCH_GTE: begin
-                if ($signed(rs1) >= $signed(rs2)) begin
-                    branch_address = target_brnch;
-                    branch_scs = BRANCH_ENABLE;
-                end
-            end
-            INST_BRCH_LSTU: begin
-                if (rs1 < rs2) begin
-                    branch_address = target_brnch;
-                    branch_scs = BRANCH_ENABLE;
-                end
-            end
-            INST_BRCH_GTEU: begin
-                if (rs1 >= rs2) begin
-                    branch_address = target_brnch;
-                    branch_scs = BRANCH_ENABLE;
-                end
-            end
-            default: begin
-                branch_address = '0;
-                branch_scs = BRANCH_DISABLE;
-            end
-        endcase
+            case (branch_op)
+                INST_BRCH_EQ: branch_scs = (rs1 == rs2) ? BRANCH_ENABLE : BRANCH_DISABLE;
+                INST_BRCH_NEQ: branch_scs = (rs1 != rs2) ? BRANCH_ENABLE : BRANCH_DISABLE;
+                INST_BRCH_LST: branch_scs = ($signed(rs1) < $signed(rs2)) ? BRANCH_ENABLE : BRANCH_DISABLE;
+                INST_BRCH_GTE: branch_scs = ($signed(rs1) >= $signed(rs2)) ? BRANCH_ENABLE : BRANCH_DISABLE;
+                INST_BRCH_LSTU: branch_scs = (rs1 < rs2) ? BRANCH_ENABLE : BRANCH_DISABLE;
+                INST_BRCH_GTEU: branch_scs = (rs1 >= rs2) ? BRANCH_ENABLE : BRANCH_DISABLE;
+                default: branch_scs = BRANCH_DISABLE;
+            endcase
+
+            branch_address = (branch_scs == BRANCH_ENABLE) ? target_brnch : 'b0;
+        end else begin
+            target_brnch = 'b0;
+            branch_scs = BRANCH_DISABLE;
+            branch_address = 'b0;
+        end
     end
 
 endmodule
